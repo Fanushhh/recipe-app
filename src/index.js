60464b68724ef252f518deb92ef171c96646da19
Fanush <fgabriel.mihailescu@gmail.com>
2023-02-21 19:58:40 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import RecipePage from './components/RecipePage/RecipePage';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename='/recipe-app'>
-      <Switch>
+      <Routes>
         <Route path='/' element={<SharedLayout />}>
           <Route index element={<App />} />
           <Route path=':recipeId' element={<RecipePage />} />
         </Route>
-      </Switch>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
